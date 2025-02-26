c4239e63aa32ab1d600522abb592f20c805f76b1
ChrisD <cdiazhernandez@toromail.csudh.edu>
2025-02-26 12:43:02 -0800

    Added experience section

---
+++ src/app/components/resume/education.tsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+export default function Education(){
+    return(
+        <div className="p-1">
+            <h5 className="mb-2 text-m font-bold tracking-tight">Education</h5>
+            <hr className="h-px my-8 bg-black border-0 separator mt-0 mb-0" />
+            <div className="mb-3 font-normal">
+                <strong>California State University, Dominguez Hills</strong>
+                <p className="text-sm font-light">Bachelor of Science, CSC</p>
+                <p className="text-sm font-light italic">June 2024 - May 2028</p>
+                <p className="text-sm font-light font">
+                    At California State University, Dominguezz Hills, I am pursuing a Bachelors of Science degree in Computer Science.</p>
+            </div>
+        </div>       
+    );
+}
